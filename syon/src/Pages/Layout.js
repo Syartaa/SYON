17ad8d8e0d1ac2d03ede5f5beb13a3dc0020048e
Syartaa <syartapajaziti@gmail.com>
2024-05-28 20:08:54 +0200

    changing the navbar

--- syon/src/Pages/Layout.js
+++ syon/src/Pages/Layout.js
@@ -6,7 +6,7 @@ import Footer from '../components/Footer'
 
 export default function Layout() {
   return (
-    <div>
+    <div className='relative min-h-screen'>
         <Navbar/>
         <Outlet/>
         <Footer/>

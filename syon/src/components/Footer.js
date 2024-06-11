20c6c486fc56974441bb635f957c628b993b89ad
Syartaa <syartapajaziti@gmail.com>
2024-06-12 00:00:42 +0200

    updating

--- syon/src/components/Footer.js
+++ syon/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="bg-gradient-to-r from-red-500 to-orange-500 text-white p-4 text-center">
       <div className="container mx-auto">
         <p>&copy; {new Date().getFullYear()} Syon Gallery. All Rights Reserved.</p>
       </div>
--- syon/src/components/Navbar.js
+++ syon/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import logo from '../image/s.png'
+import logo from '../image/s.png';
 
 const Navbar = () => {
   // State to handle the visibility of the mobile menu
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <nav className="border-gray-200 bg-gradient-to-r from-red-500 to-orange-500 dark:border-gray-700">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4 ml-5 mr-5">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-15 w-20" alt="Logo" />
@@ -26,18 +26,18 @@ const Navbar = () => {
         </button>
         {/* Mobile menu */}
         <div className={`w-full ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="navbar-hamburger">
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-transparent dark:bg-transparent">
             <li>
-              <a href="/home" className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" aria-current="page">Home</a>
+              <a href="/home" className="block py-2 px-3 text-white hover:bg-red-600  rounded  dark:text-white" >Home</a>
             </li>
             <li>
-              <a href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">About us</a>
+              <a href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-red-600 hover:text-white dark:text-gray-400  dark:hover:text-white">About us</a>
             </li>
             <li>
-              <a href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Contact</a>
+              <a href="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-red-600 hover:text-white md:dark:hover:text-white dark:text-gray-400  dark:hover:text-white">Contact</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Contact</a>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-red-600 hover:text-white dark:text-gray-400  dark:hover:text-white">Contact</a>
             </li>
           </ul>
         </div>
